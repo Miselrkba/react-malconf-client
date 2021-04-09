72e33afc356756aa09df3aac479efdbd02eba7ec
iii <miselrkba@gmail.com>
2021-04-10 00:41:31 +0200

    fix: added correct font adress and animate.css

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app';
 import './style.css';
+import 'animate.css';
 
 ReactDOM.render(
   <React.StrictMode>
