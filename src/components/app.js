6a1c47a55f4f7d719e13350e25b2a569ecb5f8c8
iii <miselrkba@gmail.com>
2021-04-11 11:24:14 +0200

    feat: added extracting state when fetching data

--- src/components/app.js
+++ src/components/app.js
@@ -11,13 +11,35 @@ import Results from './results';
 const App = () => {
   const [data, setData] = useState([]);
 
-  const extractConfig = () => {
-    axios
-      .get('https://malconf-server-node.herokuapp.com/')
-      .then((response) => setData(response.data))
-      .catch((error) => console.log(error));
+  const initialState = {
+    loadingFile: 'extracting...',
+    campaign: 'extracting...',
+    connectionDelay: 'extracting...',
+    connectionInterval: 'extracting...',
+    detected: 'extracting...',
+    runningPreProcessor: 'extracting...',
+    version: 'extracting...',
+    installName: 'extracting...',
+    keylogFile: 'extracting...',
+    mutex: 'extracting...',
+    ip: 'extracting...',
+    password: 'extracting...',
+    port: 'extracting...',
   };
 
+  const extractConfig = async () => {
+    setData(initialState);
+    const response = await axios.get(
+      'https://malconf-server-node.herokuapp.com/'
+    );
+    try {
+      setData(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  console.log(data);
   return (
     <div id="wrapper">
       <Header />
